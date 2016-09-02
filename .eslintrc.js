2b123728933403d99fde8b587d89631369ede779
Daniel Nehring <nehrdani@gmail.com>
2016-09-02 21:17:45 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "env": {
+    "browser": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [1, 2],
+    "quotes": [1, "double"],
+    "semi": [2, "always"]
+  }
+};
