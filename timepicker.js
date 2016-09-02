8637c7c3e96bf72711e29f9b8ec0b26065fac723
Daniel Nehring <nehrdani@gmail.com>
2016-09-02 21:24:30 +0200

    create layout

---
+++ timepicker.js
@@ -0,0 +1,13 @@
+/*!
+ * Timepicker
+ *
+ * Copyright © 2016 Daniel Nehring | MIT license | https://github.com/NehrDani/Timepicker
+ */
+
+(function (window) {
+  window.Timepicker = Timepicker;
+
+  function Timepicker () {
+
+  }
+})(window);
